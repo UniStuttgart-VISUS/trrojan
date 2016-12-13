93499baf11be557775a0baafd702bd8476422bd7
Christoph <christoph.mueller@visus.uni-stuttgart.de>
2016-12-13 16:15:22 +0100

    CMakeFiles now working

---
+++ trrojancore/include/trrojan/export.h
@@ -0,0 +1,13 @@
+/// <copyright file="export.h" company="SFB-TRR 161 Quantitative Methods for Visual Computing">
+/// Copyright © 2016 SFB-TRR 161. Alle Rechte vorbehalten.
+/// </copyright>
+/// <author>Christoph Müller</author>
+
+#pragma once
+
+
+#ifdef TRROJANCORE_EXPORTS
+#define TRROJANCORE_API __declspec(dllexport)
+#else /* TRROJANCORE_EXPORTS */
+#define TRROJANCORE_API __declspec(dllimport)
+#endif /* TRROJANCORE_EXPORTS*/
