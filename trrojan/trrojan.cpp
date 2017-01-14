ae1c970c68e7c8f146f6e613ff5e57a6673999c5
crowbar27 <christophmueller@web.de>
2017-01-14 17:50:33 +0100

    Added computer and user information in system_factors. Added detection of elevation in system_factors.

--- trrojan/trrojan.cpp
+++ trrojan/trrojan.cpp
@@ -11,40 +11,6 @@
 int main(const int argc, const char **argv) {
     trrojan::cmd_line cmdLine(argv, argv + argc);
 
-    //std::vector<int> values1 = { 1, 2, 3, 4, 5 };
-    //auto factor1 = trrojan::factor::from_manifestations("factor1", values1);
-    //std::cout << factor1.name() << " " << factor1.size() << std::endl;
-    //for (size_t i = 0; i < factor1.size(); ++i) {
-    //    std::cout << factor1[i] << std::endl;
-    //}
-    //for (auto it = trrojan::factorator(factor1), end = trrojan::factorator(factor1, true); it != end; ++it) {
-    //    std::cout << *it << std::endl;
-    //}
-
-    //auto factor2 = trrojan::factor::from_manifestations("factor2", values1.begin(), values1.end());
-    //std::cout << factor2.name() << " " << factor2.size() << std::endl;
-    //for (size_t i = 0; i < factor2.size(); ++i) {
-    //    std::cout << factor2[i] << std::endl;
-    //}
-
-    //auto factor3 = trrojan::factor::from_manifestations("factor3", std::move(values1));
-    //std::cout << factor3.name() << " " << factor3.size() << std::endl;
-    //for (size_t i = 0; i < factor3.size(); ++i) {
-    //    std::cout << factor3[i] << std::endl;
-    //}
-
-    //auto factor4 = trrojan::factor::from_steps("factor4", int(10), int(5), 3);
-    //std::cout << factor4.name() << " " << factor4.size() << std::endl;
-    //for (size_t i = 0; i < factor4.size(); ++i) {
-    //    std::cout << factor4[i] << std::endl;
-    //}
-
-    //auto factor5 = trrojan::factor::from_range("factor5", int(0), int(10), 4);
-    //std::cout << factor5.name() << " " << factor5.size() << std::endl;
-    //for (size_t i = 0; i < factor5.size(); ++i) {
-    //    std::cout << factor5[i] << std::endl;
-    //}
-
     trrojan::timer t;
     t.start();
 
