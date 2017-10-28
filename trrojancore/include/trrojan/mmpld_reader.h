336315f68fdf920435588f9296c25b0967b7283b
crowbar27 <christophmueller@web.de>
2017-10-28 13:39:41 +0200

    Added mmpld_reader from gigaridis.

---
+++ trrojancore/include/trrojan/mmpld_reader.h
@@ -0,0 +1,178 @@
+/// <copyright file="mmpld_reader.h" company="SFB-TRR 161 Quantitative Methods for Visual Computing">
+/// Copyright © 2017 SFB-TRR 161. Alle Rechte vorbehalten.
+/// </copyright>
+/// <author>Christoph Müller</author>
+
+#pragma once
+
+#include "trrojan/export.h"
+
+#include <cstdint>
+#include <fstream>
+#include <vector>
+
+
+namespace trrojan {
+
+    /// <summary>
+    /// Provides help for reading MegaMol binary particle files (mmpld).
+    /// </summary>
+    /// <remarks>
+    /// https://svn.vis.uni-stuttgart.de/trac/megamol/wiki/mmpld
+    /// The reader does not support three-component colour formats and
+    /// the 16-bit position format.
+    /// </remarks>
+    class TRROJANCORE_API mmpld_reader {
+
+    public:
+
+        /// <summary>
+        /// The type of the per-vertex data stored in the file.
+        /// </summary>
+        enum class vertex_type : std::uint8_t {
+
+            /// <summary>
+            /// No positions are available on a per-vertex base.
+            /// </summary>
+            none = 0,
+
+            /// <summary>
+            /// 3D position.
+            /// </summary>
+            float_xyz = 1,
+
+            /// <summary>
+            /// 3D position and radius.
+            /// </summary>
+            float_xyzr = 2,
+
+            /// <summary>
+            /// 16-bit 3D position.
+            /// </summary>
+            short_xyz = 3
+        };
+
+        /// <summary>
+        /// The type of the colour data stored in the file.
+        /// </summary>
+        enum class colour_type : std::uint8_t {
+            none = 0,
+            uint8_rgb = 1,
+            uint8_rgba = 2,
+            float_i = 3,
+            float_rgb = 4,
+            float_rgba = 5
+        };
+
+#pragma pack(push, 1)
+        /// <summary>
+        /// The header at the beginning of any mmpld file.
+        /// </summary>
+        struct file_header {
+            char magic_identifier[6];
+            std::uint16_t version;
+            std::uint32_t frames;
+            float bounding_box[6];
+            float clipping_box[6];
+        };
+        static_assert(sizeof(file_header) == 60, "FileHeader is correctly "
+            "aligned to match the implementation in MegaMol.");
+#pragma pack(pop)
+
+        static_assert(sizeof(vertex_type) == 1, "vertex_type memory footprint "
+            "is one byte.");
+        static_assert(sizeof(colour_type) == 1, "colour_type memory footprint "
+            "is one byte.");
+
+        /// <summary>
+        /// The descriptor of the content of a particle list.
+        /// </summary>
+        struct list_header {
+
+            /// <summary>
+            /// The constant colour of the particles if
+            /// <see cref="list_header::colour_type" /> is
+            /// <see cref="colour_type::none" />.
+            /// </summary>
+            float colour[4];
+
+            /// <summary>
+            /// The type of per-vertex colours in the list.
+            /// </summary>
+            colour_type colour_type;
+
+            /// <summary>
+            /// The maximum intensity found in the list if
+            /// <see cref="list_header::colour_type" /> is
+            /// <see cref="colour_type::float_i" />.
+            /// </summary>
+            float max_intensity;
+
+            /// <summary>
+            /// The minimum intensity found in the list if
+            /// <see cref="list_header::colour_type" /> is
+            /// <see cref="colour_type::float_i" />.
+            /// </summary>
+            float min_intensity;
+
+            /// <summary>
+            /// The number of particles in the list.
+            /// </summary>
+            std::uint64_t particles;
+
+            /// <summary>
+            /// The global radius of the particles in the list if no individual
+            /// radius is given on a per-vertex basis.
+            /// </summary>
+            float radius;
+
+            /// <summary>
+            /// The type of position data per vertex.
+            /// </summary>
+            vertex_type vertex_type;
+        };
+
+        /// <summary>
+        /// The seek table which contains the offsets of individual particle lists
+        /// in the file.
+        /// </summary>
+        typedef std::vector<std::uint64_t> seek_table;
+
+        /// <summary>
+        /// Computes the particle stride from the given <see cref="list_header" />.
+        /// </summary>
+        static std::uint64_t calc_stride(const list_header& header);
+
+        //static std::vector<D3D11_INPUT_ELEMENT_DESC> ParseListHeader(
+        //    const ListHeader& header);
+
+        /// <summary>
+        /// Splits the 16-bit representation of the file version into major and
+        /// minor version.
+        /// </summary>
+        static void parse_version(int& outMajor, int& outMinor,
+            const std::uint16_t version);
+
+        /// <summary>
+        /// Opens the given MMPLD file and reads its header and seek table.
+        /// </summary>
+        /// <remarks>
+        /// The file stream is returned after reading the header and the seek
+        /// table. The file pointer is located after these data.
+        /// </remarks>
+        static std::ifstream read_file_header(file_header& outHeader,
+            seek_table& outSeekTable, const char *path);
+
+        /// <summary>
+        /// Read a <see cref="list_header" /> from the current position in the
+        /// file stream.
+        /// </summary>
+        static void read_list_header(list_header& outHeader,
+            std::ifstream& file);
+
+        mmpld_reader(void) = delete;
+
+        ~mmpld_reader(void) = delete;
+    };
+
+}
---
+++ trrojancore/src/mmpld_reader.cpp
@@ -0,0 +1,226 @@
+/// <copyright file="mmpld_reader.h" company="SFB-TRR 161 Quantitative Methods for Visual Computing">
+/// Copyright © 2017 SFB-TRR 161. Alle Rechte vorbehalten.
+/// </copyright>
+/// <author>Christoph Müller</author>
+
+#include "trrojan/mmpld_reader.h"
+
+
+/*
+ * trrojan::mmpld_reader::calc_stride
+ */
+std::uint64_t trrojan::mmpld_reader::calc_stride(const list_header& header) {
+    std::uint64_t retval = 0;
+
+    switch (header.vertex_type) {
+        case vertex_type::float_xyz:
+            retval += 12;
+            break;
+
+        case vertex_type::float_xyzr:
+            retval += 16;
+            break;
+
+        case vertex_type::short_xyz:
+            retval += 6;
+            break;
+    }
+
+    switch (header.colour_type) {
+        case colour_type::uint8_rgb:
+            retval += 3;
+            break;
+
+        case colour_type::uint8_rgba:
+            retval += 4;
+            break;
+
+        case colour_type::float_i:
+            retval += 4;
+            break;
+
+        case colour_type::float_rgb:
+            retval += 12;
+            break;
+
+        case colour_type::float_rgba:
+            retval += 16;
+            break;
+    }
+
+    return retval;
+}
+
+
+///*
+// * mmpld_reader::ParseListHeader
+// */
+//std::vector<D3D11_INPUT_ELEMENT_DESC> mmpld_reader::ParseListHeader(
+//        const ListHeader& header) {
+//    D3D11_INPUT_ELEMENT_DESC element;
+//    UINT offset = 0;
+//    std::vector<D3D11_INPUT_ELEMENT_DESC> retval;
+//
+//    the::zero_memory(&element);
+//    element.SemanticName = "POSITION";
+//    element.AlignedByteOffset = offset;
+//    element.InputSlotClass
+//        = D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA;
+//
+//    switch (header.Types.vertex_type) {
+//        case vertex_type::FLOAT_XYZ:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT;
+//            offset += 3 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//
+//        case vertex_type::FLOAT_XYZR:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R32G32B32A32_FLOAT;
+//            offset += 4 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//
+//        case vertex_type::SHORT_XYZ:
+//            THROW_THE_EXCEPTION(the::argument_exception, _T("SHORT_XYZ is")
+//                _T("unsupported."));
+//            break;
+//    }
+//
+//    the::zero_memory(&element);
+//    element.SemanticName = "COLOR";
+//    element.AlignedByteOffset = offset;
+//    element.InputSlotClass
+//        = D3D11_INPUT_CLASSIFICATION::D3D11_INPUT_PER_VERTEX_DATA;
+//
+//    switch (header.Types.colour_type) {
+//        case colour_type::FLOAT_I:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R32_FLOAT;
+//            offset += 1 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//
+//        case colour_type::FLOAT_RGB:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R32G32B32_FLOAT;
+//            offset += 3 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//
+//        case colour_type::FLOAT_RGBA:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R32G32B32A32_FLOAT;
+//            offset += 4 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//
+//        case colour_type::UINT8_RGB:
+//            THROW_THE_EXCEPTION(the::argument_exception, _T("UINT8_RGB is")
+//                _T("unsupported."));
+//            break;
+//
+//        case colour_type::UINT8_RGBA:
+//            element.Format = DXGI_FORMAT::DXGI_FORMAT_R8G8B8A8_UNORM;
+//            offset += 4 * sizeof(float);
+//            retval.push_back(element);
+//            break;
+//    }
+//
+//    return std::move(retval);
+//}
+
+
+/*
+ * trrojan::mmpld_reader::parse_version
+ */
+void trrojan::mmpld_reader::parse_version(int& outMajor, int& outMinor,
+        const std::uint16_t version) {
+    std::div_t v = std::div(version, 100);
+    outMajor = v.quot;
+    outMinor = v.rem;
+}
+
+
+/*
+ * trrojan::mmpld_reader::read_file_header
+ */
+std::ifstream trrojan::mmpld_reader::read_file_header(file_header& outHeader,
+        seek_table& outSeekTable, const char *path) {
+    std::uint64_t offset = 0;
+
+    if (path == nullptr) {
+        throw std::invalid_argument("The path to the MMPLD file must not be a "
+            "null pointer.");
+    }
+
+    // Open the file.
+    std::ifstream retval(path, std::ios::binary);
+
+    // Read and check the header.
+    retval.read(reinterpret_cast<char *>(&outHeader), sizeof(outHeader));
+    if (::strcmp(outHeader.magic_identifier, "MMPLD") != 0) {
+        throw std::runtime_error("The given stream does not start with a valid "
+            "MMPLD header.");
+    }
+
+    // Read the seek table.
+    outSeekTable.clear();
+    outSeekTable.reserve(outHeader.frames);
+    for (uint32_t i = 0; i < outHeader.frames; ++i) {
+        retval.read(reinterpret_cast<char *>(&offset), sizeof(offset));
+        outSeekTable.push_back(offset);
+    }
+
+    return retval;
+}
+
+
+/*
+ * trrojan::mmpld_reader::read_list_header
+ */
+void trrojan::mmpld_reader::read_list_header(list_header& outHeader,
+        std::ifstream& file) {
+    file.read(reinterpret_cast<char *>(&outHeader.vertex_type),
+        sizeof(outHeader.vertex_type));
+    file.read(reinterpret_cast<char *>(&outHeader.colour_type),
+        sizeof(outHeader.colour_type));
+
+    switch (outHeader.vertex_type) {
+        case vertex_type::float_xyz:
+        case vertex_type::short_xyz:
+            file.read(reinterpret_cast<char *>(&outHeader.radius),
+                sizeof(outHeader.radius));
+            break;
+
+        default:
+            outHeader.radius = -1.0f;
+            break;
+    }
+
+    switch (outHeader.colour_type) {
+        case colour_type::none: {
+            std::uint8_t rgba[4];
+            file.read(reinterpret_cast<char *>(rgba), sizeof(rgba));
+            for (size_t i = 0; i < 4; ++i) {
+                outHeader.colour[i] = static_cast<float>(rgba[i])
+                    / static_cast<float>(UCHAR_MAX);
+            }
+            outHeader.min_intensity = 0.0f;
+            outHeader.max_intensity = -1.0f;
+        } break;
+
+        case colour_type::float_i:
+            ::memset(&outHeader.colour, 0, sizeof(outHeader.colour));
+            file.read(reinterpret_cast<char *>(&outHeader.min_intensity),
+                sizeof(outHeader.min_intensity));
+            file.read(reinterpret_cast<char *>(&outHeader.max_intensity),
+                sizeof(outHeader.max_intensity));
+            break;
+
+        default:
+            ::memset(&outHeader.colour, 0, sizeof(outHeader.colour));
+            outHeader.min_intensity = 0.0f;
+            outHeader.max_intensity = -1.0f;
+            break;
+    }
+
+    file.read(reinterpret_cast<char *>(&outHeader.particles),
+        sizeof(outHeader.particles));
+}
