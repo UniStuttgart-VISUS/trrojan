03381e0b04373d3fc15be198bf25013350a9ec20
mueller <christoph.mueller@visus.uni-stuttgart.de>
2018-04-07 14:59:17 +0200

    Added empty compute shader benchmark.

---
+++ trrojand3d11/include/trrojan/d3d11/cs_volume_benchmark.h
@@ -0,0 +1,47 @@
+/// <copyright file="cs_volume_benchmark.h" company="SFB-TRR 161 Quantitative Methods for Visual Computing">
+/// Copyright © 2018 SFB-TRR 161. Alle Rechte vorbehalten.
+/// </copyright>
+/// <author>Christoph Müller</author>
+
+#pragma once
+
+#include "trrojan/d3d11/volume_benchmark_base.h"
+
+
+
+namespace trrojan {
+namespace d3d11 {
+
+    /// <summary>
+    /// Base class for D3D11 benchmarks.
+    /// </summary>
+    class TRROJAND3D11_API cs_volume_benchmark : public volume_benchmark_base {
+
+    public:
+
+        typedef volume_benchmark_base::frame_type frame_type;
+        typedef volume_benchmark_base::info_type info_type;
+        typedef volume_benchmark_base::manoeuvre_step_type manoeuvre_step_type;
+        typedef volume_benchmark_base::manoeuvre_type manoeuvre_type;
+        typedef volume_benchmark_base::point_type point_type;
+        typedef volume_benchmark_base::reader_type reader_type;
+        typedef volume_benchmark_base::step_size_type step_size_type;
+        typedef volume_benchmark_base::viewport_type viewport_type;
+
+        cs_volume_benchmark(void);
+
+        virtual ~cs_volume_benchmark(void) = default;
+
+    protected:
+
+        /// <inheritdoc />
+        virtual trrojan::result on_run(d3d11::device& device,
+            const configuration& config,
+            const std::vector<std::string>& changed);
+
+    private:
+
+    };
+
+} /* end namespace d3d11 */
+} /* end namespace trrojan */
