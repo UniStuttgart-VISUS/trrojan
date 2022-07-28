790611297002cda5da9d4f565b3e2a3b047ea6f3
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2022-07-28 15:22:28 +0200

    Fix D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE not being available everywhere.

--- trrojand3d12/include/trrojan/d3d12/utilities.h
+++ trrojand3d12/include/trrojan/d3d12/utilities.h
@@ -258,8 +258,8 @@ namespace d3d12 {
     /// <c>D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE</c>.</param>
     /// <returns></returns>
     ATL::CComPtr<ID3D12Resource> create_viridis_colour_map(device& device,
-        ID3D12GraphicsCommandList *cmd_list, const D3D12_RESOURCE_STATES state
-        = D3D12_RESOURCE_STATE_ALL_SHADER_RESOURCE);
+        ID3D12GraphicsCommandList *cmd_list,
+        const D3D12_RESOURCE_STATES state = (0x40 | 0x80));
 
     /// <summary>
     /// Gets the device the child belongs to.
