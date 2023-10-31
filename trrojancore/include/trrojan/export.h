121422e51d95ec0b0d016441e4feb212278475e2
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2023-10-31 17:15:46 +0100

    New guard style.

--- trrojancore/include/trrojan/export.h
+++ trrojancore/include/trrojan/export.h
@@ -1,22 +1,22 @@
-/// <copyright file="export.h" company="Visualisierungsinstitut der Universität Stuttgart">
-/// Copyright © 2016 - 2018 Visualisierungsinstitut der Universität Stuttgart. Alle Rechte vorbehalten.
-/// Licensed under the MIT licence. See LICENCE.txt file in the project root for full licence information.
-/// </copyright>
-/// <author>Christoph Müller</author>
+ï»¿// <copyright file="export.h" company="Visualisierungsinstitut der UniversitÃ¤t Stuttgart">
+// Copyright Â© 2016 - 2023 Visualisierungsinstitut der UniversitÃ¤t Stuttgart. Alle Rechte vorbehalten.
+// Licensed under the MIT licence. See LICENCE.txt file in the project root for full licence information.
+// </copyright>
+// <author>Christoph MÃ¼ller</author>
 
 #pragma once
 
 
 #if (defined(_MSC_VER) && !defined(TRROJANCORE_STATIC))
 
-#ifdef TRROJANCORE_EXPORTS
+#if defined(TRROJANCORE_EXPORTS)
 #define TRROJANCORE_API __declspec(dllexport)
-#else /* TRROJANCORE_EXPORTS */
+#else /* defined(TRROJANCORE_EXPORTS) */
 #define TRROJANCORE_API __declspec(dllimport)
-#endif /* TRROJANCORE_EXPORTS*/
+#endif /* defined(TRROJANCORE_EXPORTS) */
 
-#else /* (defined(_MSC_VER) && defined(TRROJANCORE_DLL)) */
+#else /* (defined(_MSC_VER) && !defined(TRROJANCORE_STATIC)) */
 
 #define TRROJANCORE_API
 
-#endif /*  (defined(_MSC_VER) && !defined(TRROJANCORE_STATIC)) */
+#endif /* (defined(_MSC_VER) && !defined(TRROJANCORE_STATIC)) */
