73516c5e349e88ea944088cc055ce1c258620c7f
Christoph Müller <christoph.mueller@visus.uni-stuttgart.de>
2024-08-20 16:57:44 +0200

    Update bench script.

--- trroll/dstorage/dstorage-benchmarks.ps1
+++ trroll/dstorage/dstorage-benchmarks.ps1
@@ -8,6 +8,7 @@ $scripts = @("C:\Users\mueller\source\repos\TRRojan\trroll\dstorage\d3d11static.
 $scripts = @("C:\Users\mueller\source\repos\TRRojan\trroll\dstorage\stream.trroll", "C:\Users\mueller\source\repos\TRRojan\trroll\dstorage\streamds.trroll")
 $scripts = @((Join-Path $trrollroot "d3d11static.trroll"), (Join-Path $trrollroot "d3d12static.trroll"), (Join-Path $trrollroot "dstoragebatches.trroll"), (Join-Path $trrollroot "dstoragegdeflate.trroll"), (Join-Path $trrollroot "dstoragenaive.trroll"), (Join-Path $trrollroot "ramstream.trroll"), (Join-Path $trrollroot "stream.trroll"), (Join-Path $trrollroot "streamds.trroll"))
 $scripts = @((Join-Path $trrollroot "stream.trroll"), (Join-Path $trrollroot "streamds.trroll"))
+$scripts = @((Join-Path $trrollroot "dstoragesplitbatches.trroll"))
 
 
 $scripts | %{
